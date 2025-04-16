22abd34cd9d1befd47d46d21044e78fae8a42ab2
Nathaniel Downey <downeyn21@gmail.com>
2025-04-16 09:57:28 -0400

    updated server

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const prisma = require("prisma")
 
+app.use(express.json())
 
+//=============user routes===================
+const userRouter = require('.src/routes/user')
+app.use("/user", userRouter)
+
+//=============review routes=================
+const reviewRouter = require(".src/routes/review")
+app.use("/review", reviewRouter)
 
 
 app.listen(3000, ()=> {
