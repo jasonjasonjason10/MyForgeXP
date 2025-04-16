03e4abeb7d75e37c824ccc4afa211f2b1d8e4555
Taylor Hirth <taylorhirth19@gmail.com>
2025-04-16 15:19:47 -0500

    added logic to login, ugly but works

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Homepage from "./components/Homepage";
@@ -6,12 +7,15 @@ import SingleGame from "./components/Games/SingleGame";
 import Login from "./components/Login";
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
   return (
     <div className="min-h-screen bg-gray-950 text-white">
       <NavBar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/all-games" element={<AllGames />} />
         <Route path="/games/:id" element={<SingleGame />} />
         {/* Add other routes later, set "/" to Homepage so that when user goes to forgexp.com it routes to HomePage */}
--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,35 +1,72 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //===== LOGIC HERE =====
-        console.log('Logging in with:', { email, password });
-      };
+const Login = ({ setToken }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-      return (
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>Email:</label>
-            <input 
-              type="email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} 
-              required/>
-          </div>
-          <div>
-            <label>Password:</label>
-            <input 
-              type="password" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
-              required/>
-          </div>
-          <button type="submit">Login</button>
-        </form>
-      );
-    };
-    
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!email && !password) {
+      setError("This field is required.");
+      console.error(error);
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/user/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const result = await response.json();
+      setToken(result);
+      localStorage.setItem("token", result);
+      setSuccessMessage(`${result.message} Welcome ${result.email}`);
+      setEmail("");
+      setPassword("");
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Sign In</h2>
+      {error && <p>{error}</p>}
+      {successMessage && <p>{successMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+            required/>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            minLength="8"
+            required/>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
 export default Login;
