9e381ab9834a51fde1766e6bea8e44a3a61a9496
Nathaniel Downey <downeyn21@gmail.com>
2025-04-18 17:50:03 -0400

    progress

--- front-end/src/components/Account/Account.jsx
+++ front-end/src/components/Account/Account.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 
 function Account() {
+const [avatar, setAvatar] = useState();
+
+async function avatarHandle(e) { 
+  e.preventDefault();
+  
+}
+
     return (
       <div>
         <h1>My Account</h1>
         {/* Your account page content goes here */}
+
+{/* ================avatar=handling=================== */}
+      <div>
+        <form onSubmit={avatarHandle} >
+          <input type="file" accept="images/*" onChange={(e) => {e.target.files[0]}} className="border-solid border bg-amber-700"/>
+          <button type="submit" className="border-solid border m-25">Submit</button>
+        </form>
+      </div>
+{/* ====================DO NOT TOUCH================== */}
       </div>
     );
   }
