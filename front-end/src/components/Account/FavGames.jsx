327925c5c2ccd2f466514b1804ee6e07e0ef1dbb
Jason Howell <howell.jasong@gmail.com>
2025-04-21 11:23:13 -0500

    All MyAccount Skeletons

---
+++ front-end/src/components/Account/FavGames.jsx
@@ -0,0 +1,8 @@
+export default function FaveGames() {
+  return (
+    <div>
+      <h3 className="text-lg font-bold mb-2">My fav games</h3>
+      <p>HOW DO WE WANT TO DISPLAY?</p>
+    </div>
+  );
+}
---
+++ front-end/src/components/Account/Uploads.jsx
@@ -0,0 +1,9 @@
+export default function Uploads() {
+    return (
+      <div>
+        <h3 className="text-lg font-bold mb-2">My uploads. (image and clip content?)</h3>
+        <p>PICS AND VIDS, AUTOPLAY VIDS?</p>
+      
+      </div>
+    );
+  }
