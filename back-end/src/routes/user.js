52fdc54797da8094b8cff2717e8de19cf2df1683
Fernando <fernandodestrada@gmail.com>
2025-04-21 13:40:26 -0400

    created folders, small fix on users

--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -20,7 +20,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage })
 
-const defaultAvatar = '/images/defaultavatar1.png'
+const defaultAvatar = '/images/pfp/defaultavatar1.png'
 
 //============Routers to create=====================
 
