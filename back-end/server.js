72f1ae762c9770df5cc0789feb73194e1f21b6f8
Jason Howell <howell.jasong@gmail.com>
2025-04-22 09:42:59 -0500

    Username displaying

--- back-end/server.js
+++ back-end/server.js
@@ -1,40 +1,47 @@
+const path = require("path");
 const cors = require("cors");
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 // const prisma = require("prisma")
 
 app.use(cors());
-app.use(express.json())
-app.use(require('morgan')('dev'))
+app.use(express.json());
+app.use(require("morgan")("dev"));
 
 //=============user routes===================
 
-const userRouter = require('./src/routes/user')
-app.use("/user", userRouter)
+const userRouter = require("./src/routes/user");
+app.use("/user", userRouter);
 
 //=============favorite routes=================
 
-const favoritesRouter = require('./src/routes/favorites')
-app.use('/user', favoritesRouter)
+const favoritesRouter = require("./src/routes/favorites");
+app.use("/user", favoritesRouter);
 
 //=============posts routes=================
 
-const postRouter = require('./src/routes/post')
-app.use('/post', postRouter)
+const postRouter = require("./src/routes/post");
+app.use("/post", postRouter);
 
 //=============game-community routes=================
 
-const gameCommunityRouter = require('./src/routes/gameCommunity')
-app.use('/gamecommunity', gameCommunityRouter)
+const gameCommunityRouter = require("./src/routes/gameCommunity");
+app.use("/gamecommunity", gameCommunityRouter);
 
 //=============comments routes=================
 
-const commentsRouter = require('./src/routes/comments')
-app.use('/post', commentsRouter)
+const commentsRouter = require("./src/routes/comments");
+app.use("/post", commentsRouter);
 
-app.use('/images/pfp', express.static(path.join(__dirname, 'src', 'images', 'pfp')))
-app.use('/images/posts', express.static(path.join(__dirname, 'src', 'images', 'posts')))
+app.use(
+  "/images/pfp",
+  express.static(path.join(__dirname, "src", "images", "pfp"))
+);
+app.use(
+  "/images/posts",
+  express.static(path.join(__dirname, "src", "images", "posts"))
+);
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
