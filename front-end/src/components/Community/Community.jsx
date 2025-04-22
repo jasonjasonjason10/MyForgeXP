3b04b1024b92a762f647f497ab7206e9c5ffce39
Nathaniel Downey <downeyn21@gmail.com>
2025-04-22 13:54:14 -0400

    fetch on community

--- front-end/src/components/Community/Community.jsx
+++ front-end/src/components/Community/Community.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+
 function Community() {
+  const [postList, setPostList] = useState([])
+  console.log(postList)
+  useEffect(() => {
+    async function fetchPostList() {
+      const response = await fetch('http://localhost:3000/post/all')
+      const result = await response.json()
+      setPostList(result.post)
+    }
+    fetchPostList()
+  },[])
+
   return (
     <div>
       <h1>Community page</h1>
