30980fb327a1bd04f29bf12592eae4e562babfca
Nathaniel Downey <downeyn21@gmail.com>
2025-04-24 12:44:18 -0400

    init

--- front-end/src/components/Games/AllGames.jsx
+++ front-end/src/components/Games/AllGames.jsx
@@ -2,6 +2,16 @@ import { Link } from "react-router-dom";
 import mockData from "../../data/mockData";
 
 export default function AllGames() {
+
+
+
+
+
+
+
+
+
+  
   return (
     <div className="relative min-h-screen text-white overflow-hidden drop-shadow-[0_0_10px_rgba(255,255,255,0.5)]">
       <div className="relative z-10 flex flex-col min-h-screen">
