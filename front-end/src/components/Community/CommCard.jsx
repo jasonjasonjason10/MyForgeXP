15a6debd2fa11c74526ea92729b9ddc46f741cbf
Fernando <fernandodestrada@gmail.com>
2025-04-28 11:18:04 -0400

    videos play now

--- front-end/src/components/Community/CommCard.jsx
+++ front-end/src/components/Community/CommCard.jsx
@@ -44,88 +44,98 @@ const CommunityCard = ({ post, setRefreshToggle, refreshToggle }) => {
 
   async function favHandle(postId) {
     const response = await fetch(`${address}user/favorite/${postId}`, {
-      method: 'POST',
-      headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
-    })
-    setFavToggle(!favToggle)
+      method: "POST",
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+    setFavToggle(!favToggle);
   }
 
-  let favClass = 'p-4 bg-red-700'
-  if(postFav){
-    favClass = 'p-4 bg-green-700'
+  let favClass = "p-4 bg-red-700";
+  if (postFav) {
+    favClass = "p-4 bg-green-700";
   } else {
-    favClass = 'p-4 bg-red-700'
+    favClass = "p-4 bg-red-700";
 
+    let contentPath = post.content;
 
-  let contentPath = post.content
-
-  function videoSrc(contentPath) {
-    if (contentPath.startsWith('http://') || contentPath.startsWith('https://')) {
-      return contentPath
-    } else {
-      return `http://localhost:3000${contentPath}`
+    function videoSrc(contentPath) {
+      if (
+        contentPath.startsWith("http://") ||
+        contentPath.startsWith("https://")
+      ) {
+        return contentPath;
+      } else {
+        return `http://localhost:3000${contentPath}`;
+      }
     }
-  }
 
-  return (
-    <div className="bg-gray-900 border border-blue-700 rounded-2xl shadow-lg p-5 flex flex-col justify-between drop-shadow-[0_0_10px_rgba(255,255,255,0.5)]">
-      {/* Post Title */}
-      <h3 className="text-xl font-semibold text-orange-400 mb-2 drop-shadow-[0_0_5px_rgba(255,165,0,0.3)]">
-        {post.title}
-      </h3>
+    return (
+      <div className="bg-gray-900 border border-blue-700 rounded-2xl shadow-lg p-5 flex flex-col justify-between drop-shadow-[0_0_10px_rgba(255,255,255,0.5)]">
+        {/* Post Title */}
+        <h3 className="text-xl font-semibold text-orange-400 mb-2 drop-shadow-[0_0_5px_rgba(255,165,0,0.3)]">
+          {post.title}
+        </h3>
 
-      {/* Post Description */}
-      <div className="text-gray-300 mb-4 break-words whitespace-pre-wrap text-sm">
-        {post.description}
-      </div>
-      
-      {post.PostType === 'image' && (
-        <div>
-          <img className='h-[180px] w-[320px]' src={`http://localhost:3000${contentPath}`} alt="" />
+        {/* Post Description */}
+        <div className="text-gray-300 mb-4 break-words whitespace-pre-wrap text-sm">
+          {post.description}
         </div>
-      )}
 
-      {post.PostType === "video" && (
-        <div>
-          <video className='h-[180px] w-[320px]' src={videoSrc(contentPath)}></video>
-        </div>
-      )}
+        {post.PostType === "image" && (
+          <div>
+            <img
+              className="h-[180px] w-[320px]"
+              src={`http://localhost:3000${contentPath}`}
+              alt=""
+            />
+          </div>
+        )}
 
-      {/* Like Button */}
-      <div className="mt-4 flex justify-between items-center">
-        <button
-          onClick={() => likeHandle(post.id)}
-          className={`px-3 py-1 rounded-md text-xs font-semibold transition 
+        {post.PostType === "video" && (
+          <div>
+            <video
+              className="h-[180px] w-[320px]"
+              controls
+              src={videoSrc(contentPath)}
+            ></video>
+          </div>
+        )}
+
+        {/* Like Button */}
+        <div className="mt-4 flex justify-between items-center">
+          <button
+            onClick={() => likeHandle(post.id)}
+            className={`px-3 py-1 rounded-md text-xs font-semibold transition 
             ${
               postLiked
                 ? "bg-orange-500 hover:bg-orange-400"
                 : "bg-gray-600 hover:bg-gray-500 border border-orange-500"
             }`}
-        >
-          {postLiked ? " Liked" : "🤍 Like"}
-        </button>
+          >
+            {postLiked ? " Liked" : "🤍 Like"}
+          </button>
 
-        <span className="text-gray-400 text-xs">
-          Likes:{" "}
-          <span className="text-white font-bold">{post.likes.length}</span>
-        </span>
-      </div>
+          <span className="text-gray-400 text-xs">
+            Likes:{" "}
+            <span className="text-white font-bold">{post.likes.length}</span>
+          </span>
+        </div>
 
-      {/* Save Button */}
-      <div
-        onClick={() => favHandle(post.id)}
-        className={`mt-4 text-center text-xs font-semibold cursor-pointer rounded-md transition p-3 w-32 mx-auto
+        {/* Save Button */}
+        <div
+          onClick={() => favHandle(post.id)}
+          className={`mt-4 text-center text-xs font-semibold cursor-pointer rounded-md transition p-3 w-32 mx-auto
     ${
       postFav
         ? "bg-orange-500 hover:bg-orange-400 border border-blue-700"
         : " border border-blue-700 shadow-[0_0_20px_#22d3ee60] hover:shadow-blue-700 duration-300"
     }`}
-      >
-        {postFav ? "Favorited" : "Add to Favorites"}
+        >
+          {postFav ? "Favorited" : "Add to Favorites"}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
-}
 
 export default CommunityCard;
