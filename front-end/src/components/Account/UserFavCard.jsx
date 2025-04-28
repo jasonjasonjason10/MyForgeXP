2606bc57ab1941b8cb550f326d8576ddda5a4739
Taylor Hirth <taylorhirth19@gmail.com>
2025-04-28 12:04:35 -0500

    formatted uploads n favs on account, no conditionals for displaying img or vid yet

--- front-end/src/components/Account/UserFavCard.jsx
+++ front-end/src/components/Account/UserFavCard.jsx
@@ -1,11 +1,22 @@
 function UserFavCard({post}) {
-    return ( 
-        <>
-        <div>~~post#{post.id}~~</div>
-        <div>{post.title}</div>
-        <div>~~~~~~~~~~~~~~~~</div>
-        </>
+    return (
+        <div className="flex flex-col space-y-2">
+          <div className="text-xl text-orange-400 font-semibold drop-shadow-[0_0_5px_rgba(255,165,0,0.3)]">
+            {post.title}
+          </div>
+  
+          <div className="text-gray-300 break-words whitespace-pre-wrap">
+            {post.description}
+          </div>
+  
+          <div className="text-sm text-gray-500 mt-auto">
+            Posted on:{" "}
+            <span className="text-gray-400">
+              {new Date(post.createdAt).toLocaleString()}
+            </span>
+          </div>
+        </div>
     );
-}
+  }
 
 export default UserFavCard;
