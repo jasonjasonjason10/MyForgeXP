6c96461b2a5eeea842684dc99c2e7b322f861ac0
Nathaniel Downey <downeyn21@gmail.com>
2025-04-30 18:17:04 -0400

    localhost replace

--- front-end/src/components/Community/CommCard.jsx
+++ front-end/src/components/Community/CommCard.jsx
@@ -192,7 +192,8 @@ const CommunityCard = ({
             <div className="flex items-center gap-3 mb-4 hover:opacity-80 transition">
               <img
                 onClick={() => navigate(`/user/${post.user.id}`)}
-                src={`http://localhost:3000${post.user.avatar}`}
+                src={`${address}
+                ${post.user.avatar}`}
                 alt="User Avatar"
                 className="w-10 h-10 rounded-full object-cover border-2  cursor-pointer"
               />
@@ -243,7 +244,7 @@ const CommunityCard = ({
                     onClick={() => navigate(`/user/${post.user.id}`)}
                   >
                     <img
-                      src={`http://localhost:3000${post.user.avatar}`}
+                      src={`${address}{post.user.avatar}`}
                       alt="User Avatar"
                       className="w-10 h-10 rounded-full object-cover border-2"
                     />
@@ -274,7 +275,7 @@ const CommunityCard = ({
               className={`${
                 isExpanded ? "h-[120px] w-[240px]" : "h-[180px] w-[320px]"
               } object-cover mx-auto rounded-lg mb-4`}
-              src={`http://localhost:3000${contentPath}`}
+              src={`${address}${contentPath}`}
               alt=""
             />
           )}
